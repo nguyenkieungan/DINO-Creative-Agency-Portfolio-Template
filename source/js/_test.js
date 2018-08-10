bdff6932eadd80a740ff98ea0da7139ac6dfa9e6
nguyenkieungan <nguyen.kieu.ngan@framgia.com>
2018-08-10 15:00:36 +0700

    refs Task#2 fix block Testimonials

--- source/js/_test.js
+++ source/js/_test.js
@@ -1,8 +1,17 @@
 $(document).ready(function() {
-  $('.testimonials__content').slick({
+  $('.multi-slick').slick({
     infinite: true,
     slidesToShow: 2,
     slidesToScroll: 3,
+    dots:true
+  });
+});
+
+$(document).ready(function() {
+  $('.one-slick').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
     dots:true
   });
 });
